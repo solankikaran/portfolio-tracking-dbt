3aa73fc2b1a5a66821dfee969a3d0c5c25832e25
Karan Solanki <solankikaran05@gmail.com>
2025-08-21 05:01:44 +0000

    many changes

---
+++ snapshots/SNSH_ABC_BANK_POSITION.sql
@@ -0,0 +1,12 @@
+{% snapshot SNSH_ABC_BANK_POSITION %}
+{{
+    config(
+        unique_key= 'POSITION_HKEY',
+        strategy='check',
+        check_cols=['POSITION_HDIFF'],
+        invalidate_hard_deletes=True,
+    )
+}}
+SELECT *
+FROM {{ ref('STG_ABC_BANK_POSITION') }}
+{% endsnapshot %}
