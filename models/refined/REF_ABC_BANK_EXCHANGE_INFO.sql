52ab1ee8d528ed1b62febd91b3fd83f4104516c3
Karan Solanki <solankikaran05@gmail.com>
2025-08-22 13:21:46 +0000

    added exchange dim

---
+++ models/refined/REF_ABC_BANK_EXCHANGE_INFO.sql
@@ -0,0 +1,9 @@
+WITH current_from_snapshot as (
+    SELECT 
+        *
+        EXCLUDE (DBT_SCD_ID, DBT_UPDATED_AT, DBT_VALID_FROM, DBT_VALID_TO)
+    FROM {{ ref('SNSH_ABC_BANK_EXCHANGE_INFO') }}
+    WHERE DBT_VALID_TO is null
+)
+SELECT *
+FROM current_from_snapshot
