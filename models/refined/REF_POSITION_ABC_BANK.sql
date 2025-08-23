be1e1181d2f58705f3179fb40ed6dd5bd2e54ca9
Karan Solanki <solankikaran05@gmail.com>
2025-08-23 13:46:10 +0000

    replaced repeated CTEs with macro implementation

--- models/refined/REF_POSITION_ABC_BANK.sql
+++ models/refined/REF_POSITION_ABC_BANK.sql
@@ -1,10 +1,10 @@
 WITH current_from_snapshot AS (
-    SELECT *
-    FROM {{ ref('SNSH_ABC_BANK_POSITION') }}
-    WHERE DBT_VALID_TO IS NULL 
+    {{
+        current_from_snapshot(snsh_ref = ref('SNSH_ABC_BANK_POSITION'))
+    }}
 )
 SELECT 
     *,
     (POSITION_VALUE - COST_BASE) AS UNREALIZED_PROFIT,
     ROUND((UNREALIZED_PROFIT / COST_BASE), 5)*100 AS UNREALIZED_PROFIT_PCT
-FROM current_from_snapshot
+FROM current_from_snapshot 
