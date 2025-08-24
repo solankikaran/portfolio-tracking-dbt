c3c4e059599a48aa71e989a83f569286a6bfe4ed
Karan Solanki <solankikaran05@gmail.com>
2025-08-24 07:48:21 +0000

    added conditional IF macro to include/exclude field

--- macros/current_from_snapshot.sql
+++ macros/current_from_snapshot.sql
@@ -1,8 +1,11 @@
-{% macro current_from_snapshot(snsh_ref) %}
+{% macro current_from_snapshot(snsh_ref, output_load_ts = true) %}
 
     SELECT 
         *
         EXCLUDE (DBT_SCD_ID, DBT_UPDATED_AT, DBT_VALID_FROM, DBT_VALID_TO)
+        {% if output_load_ts %}
+            , DBT_UPDATED_AT AS SNSH_LOAD_TS_UTC
+        {% endif %}
     FROM {{ snsh_ref }}
     WHERE DBT_VALID_TO is null
 
